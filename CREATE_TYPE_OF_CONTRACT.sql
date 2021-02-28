eeed7361d46af7f137b56d5c39b1f7c493849fcd
Tomasz Kowalczyk <toma881@gmail.com>
2021-02-28 14:35:04 +0100

    Creating Type_Of_Contract table

---
+++ CREATE_TYPE_OF_CONTRACT.sql
@@ -0,0 +1,20 @@
+USE [MoveOrm]
+GO
+
+/****** Object:  Table [dbo].[Type_Of_Contract]    Script Date: 2/28/2021 2:34:01 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Type_Of_Contract](
+	[Contract_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nchar](10) NOT NULL,
+ CONSTRAINT [PK_Type_Of_Contract] PRIMARY KEY CLUSTERED 
+(
+	[Contract_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
