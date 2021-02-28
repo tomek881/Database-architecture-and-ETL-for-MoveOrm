fff397e8e698f8c4df24de00ab77c44dbf31a05a
Tomasz Kowalczyk <toma881@gmail.com>
2021-02-28 19:33:21 +0100

    Creating order valuables transportation table

---
+++ CREATE_ORDER_VALUABLES_TRANSPORTATION.sql
@@ -0,0 +1,27 @@
+USE [MoveOrm]
+GO
+
+/****** Object:  Table [dbo].[Order_Valuables_Transportation]    Script Date: 2/28/2021 7:32:44 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Order_Valuables_Transportation](
+	[Order_ID] [int] NOT NULL,
+	[Client_ID] [int] NOT NULL,
+	[Tax_ID] [int] NOT NULL,
+	[Insurance_ID] [int] NOT NULL,
+	[Order_Description] [ntext] NULL,
+	[Shipment_Address] [int] NOT NULL,
+	[Collection_Address] [int] NOT NULL,
+	[Declared_Value] [decimal](20, 2) NOT NULL,
+	[Transportation_Quickness] [int] NOT NULL,
+ CONSTRAINT [PK_Order_Valuables_Transportation] PRIMARY KEY CLUSTERED 
+(
+	[Order_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
