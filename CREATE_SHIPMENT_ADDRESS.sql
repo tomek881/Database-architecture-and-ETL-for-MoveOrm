6cdade7fac7d5504b3e3d2a5c691a1d46f9db042
Tomasz Kowalczyk <toma881@gmail.com>
2021-02-28 17:47:31 +0100

    Creating Shipment address table

---
+++ CREATE_SHIPMENT_ADDRESS.sql
@@ -0,0 +1,25 @@
+USE [MoveOrm]
+GO
+
+/****** Object:  Table [dbo].[Shipment_Address]    Script Date: 2/28/2021 5:46:39 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Shipment_Address](
+	[Address_ID] [int] NOT NULL,
+	[City] [nvarchar](100) NOT NULL,
+	[Post_Code] [int] NOT NULL,
+	[Street_Name] [nvarchar](100) NOT NULL,
+	[Country] [nvarchar](100) NOT NULL,
+	[Building_Number] [nvarchar](50) NULL,
+	[State] [nvarchar](100) NULL,
+ CONSTRAINT [PK_Shipment_Address] PRIMARY KEY CLUSTERED 
+(
+	[Address_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
