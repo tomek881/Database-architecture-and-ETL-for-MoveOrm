113ced8ffdbb51239e1ff1903917bbe66637101b
Tomasz Kowalczyk <toma881@gmail.com>
2021-02-28 13:02:36 +0100

    Creating Clients Table

---
+++ CREATE_CLIENTS_TABLE.sql
@@ -0,0 +1,23 @@
+USE [MoveOrm]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Clients](
+	[Client_ID] [int] NOT NULL,
+	[Name] [nvarchar](100) NULL,
+	[Second_Name] [nvarchar](100) NULL,
+	[Last_Name] [nvarchar](100) NULL,
+	[Company] [int] NULL,
+ CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED 
+(
+	[Client_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
